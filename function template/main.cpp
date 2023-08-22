29795a124e81f588b390c7fbe4e17eb596da49d6
tunguyendeleter <tunguyendeleter@gmail.com>
2023-08-22 21:37:23 +0700

    add function template folder

---
+++ function template/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+template <class tunguyendeleter>
+void HoanVi(tunguyendeleter &a, tunguyendeleter &b)
+{
+    tunguyendeleter temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    string as = "tu";
+    string bs = "ngoc";
+    float a = 5.5, b = 10.5;
+
+    HoanVi<float>(a, b);
+    cout << "\na = " << a;
+    cout << "\nb = " << b;
+
+    HoanVi<string>(as, bs);
+    cout << "\na = " << as;
+    cout << "\nb = " << bs;
+    return 0;
+}
